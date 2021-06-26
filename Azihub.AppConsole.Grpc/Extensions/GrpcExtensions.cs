a324b6bc72fe572f0c673ba189244bd472470a3a
Mason Chase <mc@maxtld.com>
2021-06-26 22:56:29 +0200

    Added Grpc Interceptors + Exception handling at server side

---
+++ Azihub.AppConsole.Grpc/Extensions/GrpcExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ProtoBuf.Grpc.Server;
+using Azihub.AppConsole.Grpc.Interceptors;
+
+namespace Azihub.AppConsole.Base.Extensions
+{
+    public static class GrpcExtensions
+    {
+        /// <summary>
+        /// This extension adds the CodeFirstGrpc and CodeFirstGrpcReflection service methods and registers the SsoRpcExceptionsInterceptor class.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddAndConfigureCodeFirstGrpc(this IServiceCollection services)
+        {
+            services.AddCodeFirstGrpc(config =>
+            {
+                config.Interceptors.Add(typeof(RpcExceptionsInterceptor));
+                config.ResponseCompressionLevel = System.IO.Compression.CompressionLevel.Optimal;
+            });
+
+            services.TryAddSingleton(RpcExceptionsInterceptor.Instance);
+            services.AddCodeFirstGrpcReflection();
+
+            return services;
+        }
+    }
+}
---
+++ Azihub.AppConsole.Tests/GrpcTests.cs
@@ -0,0 +1,16 @@
+﻿using Azihub.AppConsole.Base.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Azihub.AppConsole.Tests
+{
+    public class GrpcTests
+    {
+        [Fact]
+        public void GrpcServiceTest()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddAndConfigureCodeFirstGrpc();
+        }
+    }
+}
